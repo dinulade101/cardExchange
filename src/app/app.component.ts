32543b1eca99a06f11390e42231bdd105009ebd6
Dinula De Silva <dinulasd@gmail.com>
2017-11-18 15:45:12 -0700

    preferences added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { LaunchPage } from '../pages/launch/launch';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { PreferencesPage } from '../pages/preferences/preferences';
 
 @Component({
   templateUrl: 'app.html'
@@ -22,7 +23,8 @@ export class MyApp {
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'List', component: ListPage },
+      { title: 'Preferences', component: PreferencesPage}
     ];
 
   }
